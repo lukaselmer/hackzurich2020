9819c6342409f48490aea21a709567d9e6912c69
Lukas Elmer <lukas.elmer@gmail.com>
2020-09-19 18:49:49 +0200

    Add initial scaffold of timeline

--- app/lib/config/text_styles.dart
+++ app/lib/config/text_styles.dart
@@ -1,15 +1,20 @@
 import 'package:app/config/custom_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-final TextStyle bodyText2 = TextStyle(
+final TextStyle bodyText2 = GoogleFonts.poppins(
   fontSize: 26,
   color: white2,
-  fontFamily: 'Poppins',
 );
 
-final TextStyle bodyText1 = TextStyle(
+final TextStyle bodyText1 = GoogleFonts.poppins(
   color: yellow,
-  fontFamily: 'PoppinsEL',
+  fontWeight: FontWeight.w600,
+  fontSize: 14,
+);
+
+final TextStyle timelineText = GoogleFonts.poppins(
+  color: yellow,
   fontWeight: FontWeight.w600,
   fontSize: 14,
 );
