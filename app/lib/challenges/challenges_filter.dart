6a13cf6ea04d75e812a32f67c4053f1efbdc03d7
Marion Schleifer <marion@hasura.io>
2020-09-19 05:39:28 +0200

    extract business logic

---
+++ app/lib/challenges/challenges_filter.dart
@@ -0,0 +1,10 @@
+import 'package:app/data/challenges.dart';
+import 'package:app/models/challenge.dart';
+
+List<Challenge> myChallenges() => challenges;
+
+List<Challenge> pastChallenges() => challenges;
+
+List<Challenge> currentChallenges() => challenges;
+
+List<Challenge> upcomingChallenges() => challenges;
--- app/lib/challenges/challenges_page.dart
+++ app/lib/challenges/challenges_page.dart
@@ -1,87 +1,52 @@
-import 'package:app/challenges/app_base.dart';
-import 'package:app/data/challenges.dart';
+import 'package:app/challenges/challenges_filter.dart';
+import 'package:app/models/challenge.dart';
 import 'package:flutter/material.dart';
 
 class ChallengesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 4,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Upcoming'),
-                Tab(text: 'Current'),
-                Tab(text: 'Past'),
-                Tab(text: 'Mine'),
-              ],
-            ),
-            title: Text('Challenges'),
-          ),
-          body: TabBarView(
-            children: [
-              ListView(
-                  children: challenges
-                      .map<Widget>(
-                        (challenge) => ListTile(
-                          key: Key(challenge.id),
-                          title: Text(challenge.challengeName),
-                          onTap: () => Navigator.pushNamed(
-                            context,
-                            'challenge',
-                            arguments: challenge,
-                          ),
-                        ),
-                      )
-                      .toList()),
-              ListView(
-                  children: challenges
-                      .map<Widget>(
-                        (challenge) => ListTile(
-                          key: Key(challenge.id),
-                          title: Text(challenge.challengeName),
-                          onTap: () => Navigator.pushNamed(
-                            context,
-                            'challenge',
-                            arguments: challenge,
-                          ),
-                        ),
-                      )
-                      .toList()),
-              ListView(
-                  children: challenges
-                      .map<Widget>(
-                        (challenge) => ListTile(
-                          key: Key(challenge.id),
-                          title: Text(challenge.challengeName),
-                          onTap: () => Navigator.pushNamed(
-                            context,
-                            'challenge',
-                            arguments: challenge,
-                          ),
-                        ),
-                      )
-                      .toList()),
-              ListView(
-                  children: challenges
-                      .map<Widget>(
-                        (challenge) => ListTile(
-                          key: Key(challenge.id),
-                          title: Text(challenge.challengeName),
-                          onTap: () => Navigator.pushNamed(
-                            context,
-                            'challenge',
-                            arguments: challenge,
-                          ),
-                        ),
-                      )
-                      .toList()),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Upcoming'),
+              Tab(text: 'Current'),
+              Tab(text: 'Past'),
+              Tab(text: 'Mine'),
             ],
           ),
+          title: Text('Challenges'),
+        ),
+        body: TabBarView(
+          children: [
+            buildChallenges(context, upcomingChallenges()),
+            buildChallenges(context, currentChallenges()),
+            buildChallenges(context, pastChallenges()),
+            buildChallenges(context, myChallenges()),
+          ],
         ),
       ),
     );
+  }
+
+  Widget buildChallenges(BuildContext context, List<Challenge> challenges) {
+    if (challenges.isEmpty) return Text('');
+
+    return ListView(
+        children: challenges
+            .map<Widget>(
+              (challenge) => ListTile(
+                key: Key(challenge.id),
+                title: Text(challenge.challengeName),
+                onTap: () => Navigator.pushNamed(
+                  context,
+                  'challenge',
+                  arguments: challenge,
+                ),
+              ),
+            )
+            .toList());
   }
 }
