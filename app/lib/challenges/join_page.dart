99a6df6a1735a1328d5c4fcc54410c0a13a1ac00
Christof Buechi <christof.buechi@gmail.com>
2020-09-19 15:08:05 +0200

    initial design container

--- app/lib/challenges/join_page.dart
+++ app/lib/challenges/join_page.dart
@@ -7,7 +7,49 @@ class JoinPage extends StatelessWidget {
   const JoinPage(this.challenge);
 
   @override
-  Widget build(BuildContext context) => Container(
-        child: Text('Join'),
-      );
+  Widget build(BuildContext context) {
+    final challenge = ModalRoute.of(context).settings.arguments as Challenge;
+    return buildJoin(challenge);
+  }
+
+  Widget buildJoin(Challenge challenge) {
+    Widget rootContainer = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'You would like to join ?',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Im sure there is a way',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+    return rootContainer;
+  }
 }
