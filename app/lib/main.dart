f316b41859b71fd0230bf8fd6e8dbcf175a1f694
shvmgrx <s.goraksha@jacobs-university.de>
2020-09-19 18:38:19 +0200

    login added

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,34 +1,53 @@
-import 'package:app/challenges/challenge_page.dart';
-import 'package:app/challenges/challenges_page.dart';
+import 'package:app/resources/fbRepo.dart';
+import 'package:app/screens/loginScreen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:app/config/custom_colors.dart' as colors;
 import 'package:app/config/text_styles.dart' as styles;
-import 'package:flutter/material.dart';
+import 'challenges/challenge_page.dart';
+import 'challenges/challenges_page.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Challenge',
-        theme: ThemeData.dark().copyWith(
-          primaryColor: colors.black,
-          accentColor: colors.violet,
-          indicatorColor: Colors.white,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          floatingActionButtonTheme: FloatingActionButtonThemeData(
-            backgroundColor: colors.yellow,
-          ),
-          primaryTextTheme: TextTheme(
-            bodyText1: styles.bodyText1,
-            bodyText2: styles.bodyText2,
-          ),
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  FirebaseRepository _repository = FirebaseRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    //_repository.signOut();
+
+    return MaterialApp(
+      title: "Challenge",
+      theme: ThemeData.dark().copyWith(
+        primaryColor: colors.black,
+        accentColor: colors.violet,
+        indicatorColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: colors.yellow,
         ),
-        initialRoute: 'challenges',
-        routes: {
-          'challenges': (context) => ChallengesPage(),
-          'challenge': (context) => ChallengePage(),
+        primaryTextTheme: TextTheme(
+          bodyText1: styles.bodyText1,
+          bodyText2: styles.bodyText2,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: FutureBuilder(
+        future: _repository.getCurrentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          if (snapshot.hasData) {
+            return ChallengesPage();
+          } else {
+            return LoginScreen();
+          }
         },
-      );
+      ),
+    );
+  }
 }
--- app/lib/models/user.dart
+++ app/lib/models/user.dart
@@ -1,9 +1,45 @@
-import 'package:meta/meta.dart';
-
-@immutable
 class User {
-  final String id;
-  final String name;
+  String id;
+  String uid;
+  String name;
+  String email;
+  String username;
+  String status;
+  int state;
+  String profilePhoto;
 
-  const User({@required this.id, @required this.name});
+  User({
+    this.id,
+    this.uid,
+    this.name,
+    this.email,
+    this.username,
+    this.status,
+    this.state,
+    this.profilePhoto,
+  });
+
+  Map<String, dynamic> toMap(User user) {
+    return <String, dynamic>{
+      'id': user.id,
+      'uid': user.uid,
+      'name': user.name,
+      'email': user.email,
+      'username': user.username,
+      'status': user.status,
+      'state': user.state,
+      'profile_photo': user.profilePhoto,
+    };
+  }
+
+  User.fromMap(Map<String, dynamic> mapData) {
+    this.id = mapData['id'] as String;
+    this.uid = mapData['uid'] as String;
+    this.name = mapData['name'] as String;
+    this.email = mapData['email'] as String;
+    this.username = mapData['username'] as String;
+    this.status = mapData['status'] as String;
+    this.state = mapData['state'] as int;
+    this.profilePhoto = mapData['profile_photo'] as String;
+  }
 }
