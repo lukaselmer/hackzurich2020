a54590dcae77ec475c272dfe4c7f62f3eebe61ab
Marion Schleifer <marion.schleifer@gmail.com>
2020-09-19 06:16:54 +0200

    Challenge list screen (#1)
    * set up list view
    
    * dynamic data
    
    * set up list view
    
    * remove icons
    
    * set up list view
    
    * dynamic data
    
    * set up list view
    
    * remove icons
    
    * extract business logic
    
    * add fake button
    
    * improve list view
    
    Co-authored-by: Christof Buechi <christof.buechi@gmail.com>

--- app/lib/challenges/challenges_page.dart
+++ app/lib/challenges/challenges_page.dart
@@ -1,16 +1,54 @@
-import 'package:app/challenges/app_base.dart';
-import 'package:app/data/challenges.dart';
+import 'package:app/challenges/challenges_filter.dart';
+import 'package:app/models/challenge.dart';
 import 'package:flutter/material.dart';
 
 class ChallengesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return AppBase(ListView(
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Upcoming'),
+              Tab(text: 'Current'),
+              Tab(text: 'Past'),
+              Tab(text: 'Mine'),
+            ],
+          ),
+          title: Text('Challenges'),
+        ),
+        body: TabBarView(
+          children: [
+            buildChallenges(context, upcomingChallenges()),
+            buildChallenges(context, currentChallenges()),
+            buildChallenges(context, pastChallenges()),
+            buildChallenges(context, myChallenges()),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Fake button to add new challenge
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.cyan,
+        ),
+      ),
+    );
+  }
+
+  Widget buildChallenges(BuildContext context, List<Challenge> challenges) {
+    if (challenges.isEmpty) return Text('');
+
+    return ListView(
       children: challenges
           .map<Widget>(
             (challenge) => ListTile(
               key: Key(challenge.id),
               title: Text(challenge.challengeName),
+              subtitle: Text(challenge.teamName),
+              trailing: Text("Starts in [x] days"),
               onTap: () => Navigator.pushNamed(
                 context,
                 'challenge',
@@ -19,6 +57,6 @@ class ChallengesPage extends StatelessWidget {
             ),
           )
           .toList(),
-    ));
+    );
   }
 }
