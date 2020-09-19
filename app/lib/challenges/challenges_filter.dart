865cf587fca0bbda6e5d319dcadb43fe739ea290
Christof Buechi <christof.buechi@gmail.com>
2020-09-19 12:17:43 +0200

    filter on time

--- app/lib/challenges/challenges_filter.dart
+++ app/lib/challenges/challenges_filter.dart
@@ -3,8 +3,15 @@ import 'package:app/models/challenge.dart';
 
 List<Challenge> myChallenges() => challenges;
 
-List<Challenge> pastChallenges() => challenges;
+List<Challenge> pastChallenges() => challenges
+    .where((element) => (DateTime.now().isAfter(element.startsAt)))
+    .toList();
 
-List<Challenge> currentChallenges() => challenges;
+List<Challenge> currentChallenges() => challenges
+    .where((element) => (DateTime.now().isAfter(element.startsAt) &&
+        DateTime.now().isBefore(element.startsAt.add(element.totalDuration))))
+    .toList();
 
-List<Challenge> upcomingChallenges() => challenges;
+List<Challenge> upcomingChallenges() => challenges
+    .where((element) => (DateTime.now().isBefore(element.startsAt)))
+    .toList();
